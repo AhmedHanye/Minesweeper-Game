ca67465a94e9a238d7e92426c0155e9287cd49d3
Ahmed Hanye <ahmedhanye161004@gmail.com>
2023-09-06 06:21:16 +0300

    update

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,43 +1,40 @@
-from tkinter import * # this is for the GUI
-from tkinter import messagebox # this if for the messagebox that will show up when you win
-from random import choice # this is for choosing a random number
+from tkinter import *  # this is for the GUI
+# this if for the messagebox that will show up when you win
+from tkinter import messagebox
+from random import choice  # this is for choosing a random number
 
 
-
-
-
-## Create the main window
-root = Tk() # this is the main window
-root.title("Minesweeper Game") # this is the title of the window
+# Create the main window
+root = Tk()  # this is the main window
+root.title("Minesweeper Game")  # this is the title of the window
 root.geometry("1200x600")  # this is the size of the window
-root.config(bg="white") # this is the background color of the window
-root.iconbitmap("favicon.ico") # this is the icon of the window
+root.config(bg="white")  # this is the background color of the window
+root.iconbitmap("favicon.ico")  # this is the icon of the window
 
 
+# Menu bar functions
+def toggle_fullscreen():  # this function is for the full screen
+    # this is the attribute of the full screen
+    root.attributes("-fullscreen", not root.attributes("-fullscreen"))
 
 
-
-
-## Menu bar functions
-def toggle_fullscreen(): # this function is for the full screen
-    root.attributes("-fullscreen", not root.attributes("-fullscreen")) # this is the attribute of the full screen
-
-def new_game(): # this function is for the new game
+def new_game():  # this function is for the new game
     # this if for reseting the cells
-    for cell in Cells: 
-        cell.destroy() 
-    create_cells() 
+    for cell in Cells:
+        cell.destroy()
+    create_cells()
     # this is for reseting the sidebar
     global left_cells
-    left_cells = 20
+    left_cells = 22
     update_number_lost(left_cells)
-    global chance # this is for reseting the chance
+    global chance  # this is for reseting the chance
     global win  # this is for reseting the win
     chance = 3
     win = 0
     chance_var.set(f"You have {chance} chances")
 
-def show_help(): # this function is for the help
+
+def show_help():  # this function is for the help
     help_window = Toplevel(root)
     help_window.title("Help")
     help_text = """
@@ -57,54 +54,43 @@ def show_help(): # this function is for the help
     help_label.pack()
 
 
+# Menu bar
+menu_bar = Menu(root)  # this is for making the menu bar
+# this is for adding the new game button
+menu_bar.add_command(label="New Game", command=new_game)
+# this is for adding the full screen button
+menu_bar.add_command(label="Full Screen", command=toggle_fullscreen)
+# this is for adding the help button
+menu_bar.add_command(label="Help", command=show_help)
+# this is for adding the exit button
+menu_bar.add_command(label="Exit", command=root.quit)
+# this is for adding the menu bar to the main window
+root.config(menu=menu_bar)
 
 
-
-
-
-
-## Menu bar
-menu_bar = Menu(root) # this is for making the menu bar
-menu_bar.add_command(label="New Game", command=new_game) # this is for adding the new game button
-menu_bar.add_command(label="Full Screen", command=toggle_fullscreen) # this is for adding the full screen button
-menu_bar.add_command(label="Help" ,command=show_help) # this is for adding the help button
-menu_bar.add_command(label="Exit", command=root.quit) # this is for adding the exit button
-root.config(menu=menu_bar) # this is for adding the menu bar to the main window
-
-
-
-
-
-
-
-
-
-
-## Sidebar
+# Sidebar
 sidebar_frame = Frame(root, bg="gray", width=200)
 sidebar_frame.pack(side=LEFT, fill=Y)
 
 
-
-
-
-
-
-
-
-
-## Sidebar content
+# Sidebar content
 left_cells = 81
+
+
 def update_number_win(left_cells):
     number_label.config(text=f"Left Cells: {left_cells}")
     smiley_label.config(text="😎")
 
+
 chance = 3
+
+
 def update_number_lost(left_cells):
     number_label.config(text=f"Left Cells: {left_cells}")
     smiley_label.config(text="😊")
-    
+
     chance_var.set(f"You have {chance} chances")
+
 
 number_label = Label(sidebar_frame, text="Left Cells: 81",
                      bg="gray", fg="white", font=("Arial", 16))
@@ -121,79 +107,71 @@ number_chances = Label(
 number_chances.pack(pady=30)
 
 
+# Game over function
+def toggle_cell(event):
+    global chance  # this is for being able to change the chance variable
+    global win  # this is for being able to change the win variable
 
-
-
-
-
-
-
-## Game over function
-def toggle_cell(event): 
-    global chance # this is for being able to change the chance variable
-    global win # this is for being able to change the win variable
-    
     cell = event.widget
-    if cell["bg"] == "white": # if you click on the cell with white background
-        if cell["text"] == "0" : # if you click on the cell with 0
+    if cell["bg"] == "white":  # if you click on the cell with white background
+        if cell["text"] == "0":  # if you click on the cell with 0
             cell.config(bg="red")
-            if chance < 1:
+            if chance == 1:
                 messagebox.showinfo("Game Over", "You lost the game!")
                 new_game()
-            else: # if you have more than 1 chance
+            else:  # if you have more than 1 chance
                 chance -= 1
                 chance_var.set(f"You have {chance} chances!")
-        elif cell["text"] == "1" : # if you click on the cell with 1
+        elif cell["text"] == "1":  # if you click on the cell with 1
             cell.config(bg="black")
             global left_cells
             left_cells -= 1
             update_number_win(left_cells)
             win += 1
-    if win == 20: # if you win the game
+    if win == 22:  # if you win the game
         messagebox.showinfo("Winner", "You won the game!")
         new_game()
 
 
+# Create cells frame
+# this is the frame that contains the cells
+cells_frame = Frame(root, bg="white")
+# this is the position of the frame
+cells_frame.pack(side=LEFT, fill=BOTH, expand=True)
+
+Cells = []  # this is the list that contains the cells
+A = [0] * 3 + [1] * 22  # this is the list that contains the numbers of the cells
 
 
-
-
-## Create cells frame
-cells_frame = Frame(root, bg="white") # this is the frame that contains the cells 
-cells_frame.pack(side=LEFT, fill=BOTH, expand=True) # this is the position of the frame
-
-Cells = [] # this is the list that contains the cells
-A = [0] * 5 + [1] * 20 # this is the list that contains the numbers of the cells
-
-def create_cells(): # this function is for creating the cells
-    B = A.copy() # this is for copying the list A
-    global Cells # this is for being able to change the Cells variable
-    for row in range(5): 
+def create_cells():  # this function is for creating the cells
+    B = A.copy()  # this is for copying the list A
+    global Cells  # this is for being able to change the Cells variable
+    for row in range(5):
         for column in range(5):
-            O = choice(B) # this is for choosing a random number from the list B
+            # this is for choosing a random number from the list B
+            O = choice(B)
             cell = Label(cells_frame, text=str(O), borderwidth=1, relief="solid", font=(
-                "Arial", 16), width=2, height=1, bg="white") 
-            B.remove(O) # this is for removing the number that we chose from the list B
-            Cells.append(cell) # this is for adding the cell to the list Cells
-            cell.grid(row=row, column=column, sticky="nsew") # this is for the position of the cells
-            cells_frame.grid_columnconfigure(column, weight=1) # this is for adapting the size of the cells
-            cells_frame.grid_rowconfigure(row, weight=1) # this is for adapting the size of the cells
-            cell.bind("<Button-1>", toggle_cell) # this is for binding the left click to the toggle_cell function
-            cell.bind("<Button-3>", toggle_cell) # this is for binding the right click to the toggle_cell function
-            cell.config(fg="white", bg="white") # this is for making the text invisible
+                "Arial", 16), width=2, height=1, bg="white")
+            # this is for removing the number that we chose from the list B
+            B.remove(O)
+            Cells.append(cell)  # this is for adding the cell to the list Cells
+            # this is for the position of the cells
+            cell.grid(row=row, column=column, sticky="nsew")
+            # this is for adapting the size of the cells
+            cells_frame.grid_columnconfigure(column, weight=1)
+            # this is for adapting the size of the cells
+            cells_frame.grid_rowconfigure(row, weight=1)
+            # this is for binding the left click to the toggle_cell function
+            cell.bind("<Button-1>", toggle_cell)
+            # this is for binding the right click to the toggle_cell function
+            cell.bind("<Button-3>", toggle_cell)
+            # this is for making the text invisible
+            cell.config(fg="white", bg="white")
 
 
-create_cells() # this is for calling the create_cells() function
-new_game() # you call the function so that you don't get an error when you run the program for the first time
+create_cells()  # this is for calling the create_cells() function
+new_game()  # you call the function so that you don't get an error when you run the program for the first time
 
 
-
-
-
-
-
-
-
-
-## Exit from the game
+# Exit from the game
 root.mainloop()
